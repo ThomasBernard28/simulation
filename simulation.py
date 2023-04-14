298cb0670b16b19e2a452bb6d8c79edd6e8d7024
ThomasBernard28 <thomas.bernard@student.umons.ac.be>
2023-04-14 23:38:22 +0200

    start of a chi squared test

--- simulation.py
+++ simulation.py
@@ -1,5 +1,4 @@
 import re
-import sys
 
 
 def initialize(file):
@@ -8,10 +7,25 @@ def initialize(file):
         # delete all \n from the e string
         new_e = re.sub(r'\n', '', e)
         # First two characters are 2. and they are not decimals
-        decimals = list(new_e[2:])
+        decimals = [int(i) for i in new_e[2:]]
+        chiSquared(decimals)
+
+
+# each interval should have the same value if the data is random
+# the expected value is the number of data divided by the number of intervals
+# the observed value is the number of data in each interval
+def chiSquared(data):
+    intervals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for d in data:
+        tmp = intervals[d]
+        tmp += 1
+        intervals[d] = tmp
+    print(intervals)
 
 
 if __name__ == "__main__":
+    import sys
+
     if len(sys.argv) == 2:
         initialize(sys.argv[1])
     else:
